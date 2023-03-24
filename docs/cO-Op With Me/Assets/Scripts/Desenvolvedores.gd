4b046edb05fdbce076e87d98c02baee62c520811
KalebIsaias <kaleb.carvalho@sou.inteli.edu.br>
2023-03-24 10:14:51 -0300

    Pages

--- docs/cO-Op With Me/Assets/Scripts/Desenvolvedores.gd
+++
@@ -1,9 +0,0 @@
-extends Control
-
-# Called when the node enters the scene tree for the first time.
-func _ready():
-	pass # Replace with function body.
-
-
-func _on_TextureButton_pressed():
-	get_tree().change_scene("res://Assets/Scenes/Tela_Butões.tscn")
--- docs/cO-Op With Me/Assets/Scripts/Pensamento.gd
+++
@@ -1,6 +0,0 @@
-extends Node2D
-
-var d = Dialogic.start("pensa")
-
-func _ready():
-	add_child(d)
